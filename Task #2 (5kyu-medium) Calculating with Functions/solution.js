b76a4a457105d506e2349e6cbf4ff7caa6b8676a
Festralus <kezarnus@gmail.com>
2025-10-10 22:48:10 +0300

    Task #2: solution refactoring progress

--- Task #2 (5kyu-medium) Calculating with Functions/solution.js
+++ Task #2 (5kyu-medium) Calculating with Functions/solution.js
@@ -1,96 +1,53 @@
-function zero(action) {
-  if (action) return action(0);
-  else return 0;
+function numberGenerator(num) {
+  return function (action) {
+    return action ? action(num) : num;
+  };
 }
-function one(action) {
-  if (action) return action(1);
-  else return 1;
-}
-function two(action) {
-  if (action) return action(2);
-  else return 2;
-}
-function three(action) {
-  if (action) return action(3);
-  else return 3;
-}
-function four(action) {
-  if (action) return action(4);
-  else return 4;
-}
-function five(action) {
-  if (action) return action(5);
-  else return 5;
-}
-function six(action) {
-  if (action) return action(6);
-  else return 6;
-}
-function seven(action) {
-  if (action) return action(7);
-  else return 7;
-}
+const zero = numberGenerator(0);
+const one = numberGenerator(1);
+const two = numberGenerator(2);
+const three = numberGenerator(3);
+const four = numberGenerator(4);
+const five = numberGenerator(5);
+const six = numberGenerator(6);
+const seven = numberGenerator(7);
+const eight = numberGenerator(8);
+const nine = numberGenerator(9);
 
-function eight(action) {
-  if (action) return action(8);
-  else return 8;
-}
-function nine(action) {
-  if (action) return action(9);
-  else return 9;
-}
+// function actionGenerator(action) {
+//   if (!action) return;
+// }
 
 function plus(rightOperand) {
-  if (rightOperand >= 0 && rightOperand <= 9) {
-    return function (leftOperand) {
-      return leftOperand + rightOperand;
-    };
-  } else if (rightOperand < 0 || rightOperand > 9) {
-    console.error("Error: Please enter numbers ranging from 0 to 9");
-  } else if (rightOperand === null || rightOperand === undefined) {
-    console.error("Error: unknown variables");
-  } else {
-    console.error("Error");
-  }
+  if (!validateOperand(rightOperand)) return;
+  return function (leftOperand) {
+    return leftOperand + rightOperand;
+  };
 }
 function minus(rightOperand) {
-  if (rightOperand >= 0 && rightOperand <= 9) {
-    return function (leftOperand) {
-      return leftOperand - rightOperand;
-    };
-  } else if (rightOperand < 0 || rightOperand > 9) {
-    console.error("Error: Please enter numbers ranging from 0 to 9");
-  } else if (rightOperand === null || rightOperand === undefined) {
-    console.error("Error: unknown variables");
-  } else {
-    console.error("Error");
-  }
+  if (!validateOperand(rightOperand)) return;
+  return function (leftOperand) {
+    return leftOperand - rightOperand;
+  };
 }
+
 function times(rightOperand) {
-  if (rightOperand >= 0 && rightOperand <= 9) {
-    return function (leftOperand) {
-      return leftOperand * rightOperand;
-    };
-  } else if (rightOperand < 0 || rightOperand > 9) {
-    console.error("Error: Please enter numbers ranging from 0 to 9");
-  } else if (rightOperand === null || rightOperand === undefined) {
-    console.error("Error: unknown variables");
-  } else {
-    console.error("Error");
-  }
+  if (!validateOperand(rightOperand)) return;
+  return function (leftOperand) {
+    return leftOperand * rightOperand;
+  };
 }
+
 function dividedBy(rightOperand) {
   if (!validateOperand(rightOperand)) return;
-  if (rightOperand >= 0 && rightOperand <= 9) {
-    return function (leftOperand) {
-      return (leftOperand / rightOperand).toFixed(0);
-    };
-  }
+  return function (leftOperand) {
+    return Math.floor(leftOperand / rightOperand);
+  };
 }
 
 function validateOperand(operand) {
-  const isOperandInRange = operand >= 0 || operand <= 9;
-  const isOperandExistingValue = operand !== null || operand !== undefined;
+  const isOperandInRange = operand >= 0 && operand <= 9;
+  const isOperandExistingValue = operand !== null && operand !== undefined;
 
   if (isOperandInRange && isOperandExistingValue) return true;
   return false;
@@ -99,4 +56,4 @@ function validateOperand(operand) {
 console.log(seven(times(five()))); //  must return 35
 console.log(four(plus(nine()))); //  must return 13
 console.log(eight(minus(three()))); //  must return 5
-console.log(six(dividedBy(two()))); //  must return 3
+console.log(nine(dividedBy(three()))); //  must return 3
