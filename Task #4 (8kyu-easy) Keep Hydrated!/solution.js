05189901df1c62e20eebec84d8f0d41d9ae6cafb
Festralus <kezarnus@gmail.com>
2025-10-16 21:39:19 +0300

    Task #4 completed

---
+++ Task #4 (8kyu-easy) Keep Hydrated!/solution.js
@@ -0,0 +1,10 @@
+let num = 11.8;
+
+function litres(time) {
+  return Math.floor(time / 2);
+}
+
+console.log(litres(num));
+console.log(litres(3));
+console.log(litres(6));
+console.log(litres(1787));
