99fb830d9877eed8dcc70b1fd6968bfc9d016395
Festralus <kezarnus@gmail.com>
2026-01-10 20:51:09 +0300

    Task 18 complete

---
+++ Task #18 (6kyu-medium) Which are in/solution.js
@@ -0,0 +1,14 @@
+function inArray(array1, array2) {
+  const result = array1.filter((el) => {
+    return array2.some((word) => word.includes(el));
+  });
+
+  return result.sort();
+}
+
+console.log(
+  inArray(
+    ["xyz", "live", "strong"],
+    ["lively", "alive", "harp", "sharp", "armstrong"]
+  )
+); // expect ["live", "strong"]
