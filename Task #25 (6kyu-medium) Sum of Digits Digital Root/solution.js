9203ee1c1963d54c4893f32acd14ca4ffb4596ca
Festralus <kezarnus@gmail.com>
2026-04-30 11:13:51 +0300

    Task #25: added an alternative solution

--- Task #25 (6kyu-medium) Sum of Digits Digital Root/solution.js
+++ Task #25 (6kyu-medium) Sum of Digits Digital Root/solution.js
@@ -1,14 +1,27 @@
+// function digitalRoot(n) {
+//   const arr = (n + "").split("");
+
+//   if (arr.length === 1) return n;
+
+//   const sum = arr.reduce((acc, val) => {
+//     return +acc + +val;
+//   }, 0);
+
+//   if ((sum + "").split("").length > 1) return digitalRoot(sum);
+//   return sum;
+// }
+
 function digitalRoot(n) {
-  const arr = (n + "").split("");
+  let arr = (n + "").split("");
 
-  if (arr.length === 1) return n;
+  while (arr.length > 1) {
+    const sum = arr.reduce((acc, val) => {
+      return +acc + +val;
+    }, 0);
 
-  const sum = arr.reduce((acc, val) => {
-    return +acc + +val;
-  }, 0);
-
-  if ((sum + "").split("").length > 1) return digitalRoot(sum);
-  return sum;
+    arr = (sum + "").split("");
+  }
+  return Number(arr[0]);
 }
 
 console.log(digitalRoot(456)); // expect "6"
