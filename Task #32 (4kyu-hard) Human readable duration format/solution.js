9df83e094372343230b668f61c0372ae1fe25541
Festralus <kezarnus@gmail.com>
2026-05-29 18:53:47 +0300

    Task #32 completed

--- Task #32 (4kyu-hard) Human readable duration format/solution.js
+++ Task #32 (4kyu-hard) Human readable duration format/solution.js
@@ -8,11 +8,26 @@ function formatDuration(seconds) {
   ];
 
   const date = [];
-  const remainder = null;
+  let remainder = null;
 
-  // Cycle through units, leaving new remainders and populating date
-  // figure out the -s ending
+  remainder = seconds;
+  for (const [unitName, secondsInUnit] of units) {
+    const unitCount = Math.floor(remainder / secondsInUnit);
+
+    if (unitCount > 0) {
+      remainder %= secondsInUnit;
+
+      const label = unitCount === 1 ? unitName : unitName + "s";
+      date.push(`${unitCount} ${label}`);
+    }
+  }
+
+  if (seconds === 0) return now;
+  return date.length > 1
+    ? date.slice(0, -1).join(", ") + " and " + date.slice(-1)
+    : date[0];
 }
 
 console.log(formatDuration(62)); // expect "1 minute and 2 seconds"
 console.log(formatDuration(3662)); // expect "1 hour, 1 minute and 2 seconds"
+console.log(formatDuration(31094600662)); // expect "986 years, 1 day, 5 hours, 4 minutes and 22 seconds"
